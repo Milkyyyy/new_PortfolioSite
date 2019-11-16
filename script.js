9cb889442f6ca76dbaf4bb73b6d2a358c83968a0
今井里奈 <rina@rinanoMacBook-Pro.local>
2019-11-16 15:13:24 +0900

    workページ作成中

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
+$('.link_top').click(function() {
+
+  var top = $('#top').offset().top;
+
+  $('html, body').animate({scrollTop:top});
+
+});
+
 $('.link_aboutMe').click(function() {
-  
+
   var aboutMe_top = $('#aboutMe').offset().top;
 
   $('html, body').animate({scrollTop:aboutMe_top});
 
 });
+
+$('.link_works').click(function() {
+
+  var works_top = $('#works').offset().top;
+
+  $('html, body').animate({scrollTop:works_top});
+
+});
+
+$('.link_skill').click(function() {
+
+  var skill_top = $('#skill').offset().top;
+
+  $('html, body').animate({scrollTop:skill_top});
+
+});
+
+$('.link_activities').click(function() {
+
+  var activities_top = $('#activities').offset().top;
+
+  $('html, body').animate({scrollTop:activities_top});
+
+});
+
