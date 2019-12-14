c8896c03370ee932afeb59c0d494e0b6600032fe
今井里奈 <rina@rinanoMacBook-Pro.local>
2019-12-14 09:55:37 +0900

    全体的に改良！

--- script.js
+++ script.js
@@ -145,5 +145,23 @@ $('.poster').hover(
   }
 );
 
+setInterval( "slideSwitch()", 3000 );
 
 });
+
+function slideSwitch() {
+  var $active = $('#slideshow p.active');
+
+  if ( $active.length == 0 ) $active = $('#slideshow p:last');
+
+  var $next =  $active.next().length ? $active.next()
+    : $('#slideshow p:first');
+
+  $active.addClass('last-active');
+
+  $next.css({opacity: 0.0})
+    .addClass('active')
+    .animate({opacity: 1.0}, 1000, function() {
+          $active.removeClass('active last-active');
+    });
+}
